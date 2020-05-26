b0e923a408fb0e1c4ca98169144774f146eee9e8
ikpak <inkha.pak@gmail.com>
2020-05-26 17:14:29 +0700

    tuesday commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,107 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Box from './components/Box'
 
 const choices = {
-  fire: "https://img.pokemondb.net/artwork/large/flareon.jpg",
-  grass: "https://img.pokemondb.net/artwork/large/leafeon.jpg"
+  fire: {
+    name: "fire",
+    url: "https://img.pokemondb.net/artwork/large/flareon.jpg"
+  },
+  grass: {
+    name: "grass",
+    url: "https://img.pokemondb.net/artwork/large/leafeon.jpg"
+  },
+  water: {
+    name: "water",
+    url: "https://img.pokemondb.net/artwork/large/vaporeon.jpg"
+  }
 }
 
 function App() {
+  // let userC = {}
+  // let computerC = {}
+  let [userC, setUserC] = useState({})
+  let [computerC, setComputerC] = useState({})
+  let [previousWinner, setPreviousWinner] = useState(null)
+
+  let result = ""
+
+  const judgement = (user,computer) => {
+    if(user.name === "fire") {
+      result = computer.name === "grass" ? "Won" : "Lost"  
+    }
+    if(user.name === "grass") {
+      result = computer.name === "water" ? "Won" : "Lost"
+    }
+    if(user.name === "water") {
+      result = computer.name === "fire" ? "Won" : "Lost"
+    }
+    if(user.name === computer.name) result = "Tied"
+    console.log(result)
+
+    if(result === "Won") {
+      setPreviousWinner("You")
+    } else if(result === "Lost") {
+      setPreviousWinner("Computer")
+    } else {
+      setPreviousWinner("Tied")
+    }
+  }
+
+  const onplay = (userChoice) => {
+    console.log("User choice:", userC)
+    
+    let itemArray = Object.keys(choices)
+    let randomNum = Math.floor(Math.random() * itemArray.length)
+    let itemName = itemArray[randomNum]
+
+    console.log("Computer choice:", computerC)
+    judgement(choices[userChoice], choices[itemName])
+    setComputerC(choices[itemName])
+    setUserC(choices[userChoice])
+  }
+
   return (
     <div className="App">
-      <Box winner={true} name="Computer" imgUrl={choices.fire} />
-      <Box winner={false} name="You" imgUrl={choices.grass}  />
+      <Box name="You" choice={userC} previousWinner={previousWinner} />
+      <div class="buttons">
+        <h1>Choose your type:</h1>
+        <button onClick = {() => onplay("fire")}>Fire</button>
+        <button onClick = {() => onplay("grass")}>Grass</button>
+        <button onClick = {() => onplay("water")}>Water</button>
+      </div>
+      <Box name="Computer" choice={computerC} previousWinner={previousWinner} />
     </div>
   );
 }
 
 export default App;
+
+// rewritten using class
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props) <-- set props for parents
+//     this.state = {
+//       userC: {},
+//       computerC: {}  
+//     }
+//   }
+//
+//   onplay = (userChoice) => {
+//     let userC = 
+//     let itemArray = Object.keys(choices)
+//     let randomNum = Math.floor(Math.random() * itemArray.length)
+//     let itemName = itemArray[randomNum]
+//     
+//     this.setState({userC: choices[userChoice], computerC: choices[itemName]})
+//   }
+//   
+//   render() {
+//     return (
+//       <div className="App">
+//         <Box winner={true} name="Computer" imgUrl={this.choices.fire} />
+//         <Box winner={false} name="You" imgUrl={this.choices.grass}  />
+//       </div>
+//     )
+//   }
+// }
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,13 +1,38 @@
 import React from 'react'
 
 export default function Box(props) {
+
+    console.log("This is props", props)
+    const DEFAULT_IMG = "http://www.thewateringhole.co.uk/wp-content/uploads/2012/12/play.png";
+
+    const won = props.name === props.previousWinner
+    let className
+    const hasPreviousGame = props.previousWinner === "Computer" || props.previousWinner === "You"
+    if(hasPreviousGame) {
+        className = won ? "winner" : "loser"
+    }
+
+    let prompt
+    if(won) {
+        prompt = "Won"
+        className = won ? "winner" : "loser"
+    } else if(props.previousWinner === "Tied") {
+        prompt = "Tied"
+    } else if(props.previousWinner === null) {
+        console.log("am i here?")
+        prompt = "Start"
+    } else {
+        prompt = "Lost"
+    }
+
     return (
-
-        <div className={`box ${props.winner ? 'winner' : 'loser'}`}>
+        <div className={`box ${className}`}>
             <h2>{props.name}</h2>
-            <img src={props.imgUrl} alt={props.title} />
-            <h1>{props.winner ? 'Won' : 'Lost'}</h1>
+            <img src={props.choice.url || DEFAULT_IMG} alt={props.choice.name} />
+            <h1>{prompt}</h1>
         </div>
-
     )
 }
+
+// rewritten using class
+// export default class 
