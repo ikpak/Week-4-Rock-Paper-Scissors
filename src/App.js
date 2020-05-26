b09dc57a3acb0ce198e6af0b3df79915268e0021
ikpak <inkha.pak@gmail.com>
2020-05-27 01:28:58 +0700

    final commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import Box from './components/Box'
-
-const choices = {
-  fire: {
-    name: "fire",
-    url: "https://img.pokemondb.net/artwork/large/flareon.jpg"
-  },
-  grass: {
-    name: "grass",
-    url: "https://img.pokemondb.net/artwork/large/leafeon.jpg"
-  },
-  water: {
-    name: "water",
-    url: "https://img.pokemondb.net/artwork/large/vaporeon.jpg"
-  }
-}
+import {choices} from './utils'
 
 function App() {
   // let userC = {}
---
+++ src/utils/index.js
@@ -0,0 +1,14 @@
+export const choices = {
+    fire: {
+        name: "fire",
+        url: "https://img.pokemondb.net/artwork/large/flareon.jpg"
+    },
+    grass: {
+        name: "grass",
+        url: "https://img.pokemondb.net/artwork/large/leafeon.jpg"
+    },
+    water: {
+        name: "water",
+        url: "https://img.pokemondb.net/artwork/large/vaporeon.jpg"
+    }
+}
